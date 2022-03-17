5457e71396a8d37b07ad518f784b3f071c5291d5
CleuJunior <cleu.junior@gmail.com>
2022-03-17 00:31:14 -0300

    first exercism - hello world

---
+++ hello-world/src/main/java/Greeter.java
@@ -0,0 +1,5 @@
+class Greeter {
+
+    public static String getGreeting() { return "Hello, World!"; }
+
+}
---
+++ hello-world/src/test/java/GreeterTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GreeterTest {
+
+    @Test
+    public void testThatGreeterReturnsTheCorrectGreeting() { assertEquals("Hello, World!", new Greeter().getGreeting()); }
+
+}
