c781fd1359802b8194b6b869e1ef85acc7390fe0
Cleonildo Soares Guimaraes Junior <50106531+CleuJunior@users.noreply.github.com>
2022-03-24 14:42:03 -0300

    Play Your Cards! [Complete]

--- squeaky-clean/src/main/java/SqueakyClean.java
+++
@@ -1,5 +0,0 @@
-class SqueakyClean {
-    static String clean(String identifier) {
-        throw new UnsupportedOperationException("Please implement the (static) SqueakyClean.clean() method");
-    }
-}
