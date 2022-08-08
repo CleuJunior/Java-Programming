71b40d2a4b476b42d922824fd1df657c06364b59
Cleonildo Jr <cleu.junior@gmail.com>
2022-08-08 00:27:37 -0300

    Minesweeper [Completed]

---
+++ minesweeper/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+W
+'src/test/java/MinesweeperBoardTest.java,a\f\afa4d8d9df6d4960527889da1297ee85ff67e38b
+S
+#src/main/java/MinesweeperBoard.java,2\2\22acc90d199d8033626b9a97a43226cf99293231
+7
+HELP.md,1\a\1a273f73b7a477b0745593641bd708a6182c7ad5
--- minesweeper/src/main/java/MinesweeperBoard.java
+++ minesweeper/src/main/java/MinesweeperBoard.java
@@ -1,10 +1,78 @@
-/*
+import java.util.List;
+import java.util.ArrayList;
+public class MinesweeperBoard {
+    private final List<String> unscoredBoard;
+    private int[][] scoredBoard;
+    private int rows;
+    private int cols;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+    public MinesweeperBoard(List<String> unscoredBoard) {
+        this.unscoredBoard = unscoredBoard;
+    }
+    public List<String> withNumbers() {
+        scoreBoard();
 
-Please remove this comment when submitting your solution.
+        List<String> boardWithNumbers = new ArrayList<>();
+        for (int rowIdx = 0; rowIdx < rows; rowIdx++) {
+            StringBuilder rowBuilder = new StringBuilder();
 
-*/
+            for (int colIdx = 0; colIdx < cols; colIdx++) {
+
+                int countOrMine = scoredBoard[rowIdx][colIdx];
+                char square = ' ';
+
+                if (countOrMine == -1)
+                    square = '*';
+
+                else if (countOrMine > 0)
+                    square = (char) (countOrMine + '0');
+
+                rowBuilder.append(square);
+            }
+            boardWithNumbers.add(rowBuilder.toString());
+        }
+        return boardWithNumbers;
+    }
+    private void addMineCountToNeighbours(int rowIdx, int colIdx) {
+        for (int rowOffset = -1; rowOffset <= 1; rowOffset++) {
+            for (int colOffset = -1; colOffset <= 1; colOffset++) {
+
+                int newRowIdx = rowIdx + rowOffset;
+                int newColIdx = colIdx + colOffset;
+
+                if (newRowIdx < 0 || newRowIdx >= rows || newColIdx < 0 || newColIdx >= cols)
+                    continue;
+
+                if (scoredBoard[newRowIdx][newColIdx] == -1)
+                    continue;
+
+                scoredBoard[newRowIdx][newColIdx]++;
+            }
+        }
+    }
+    private void scoreBoard() {
+        if (this.unscoredBoard == null || this.unscoredBoard.size() == 0)
+            return;
+
+        this.rows = unscoredBoard.size();
+        this.cols = unscoredBoard.get(0).length();
+        scoredBoard = new int[rows][cols];
+
+        for (int rowIdx = 0; rowIdx < rows; rowIdx++) {
+            String row = unscoredBoard.get(rowIdx);
+
+            for (int colIdx = 0; colIdx < cols; colIdx++) {
+                char square = row.charAt(colIdx);
+                scoredBoard[rowIdx][colIdx] = square == ' ' ? 0 : -1;
+            }
+        }
+
+        for (int rowIdx = 0; rowIdx < rows; rowIdx++) {
+            for (int colIdx = 0; colIdx < cols; colIdx++) {
+
+                if (scoredBoard[rowIdx][colIdx] == -1)
+                    addMineCountToNeighbours(rowIdx, colIdx);
+            }
+        }
+    }
+}
