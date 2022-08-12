8e4f17a1fa4680b8f83a5c9b2fefd54e280d90a6
Cleonildo Jr <cleu.junior@gmail.com>
2022-08-11 22:23:17 -0300

    Rotational Cipher [Complete]

---
+++ rotational-cipher/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+W
+'src/test/java/RotationalCipherTest.java,a\5\a5b27a4b6e5a4edb750a1555b20bde9939bc95dd
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+S
+#src/main/java/RotationalCipher.java,9\3\93e029348659e3b7f64b6924c0771cf8935563db
+7
+HELP.md,1\a\1a273f73b7a477b0745593641bd708a6182c7ad5
--- rotational-cipher/src/main/java/RotationalCipher.java
+++ rotational-cipher/src/main/java/RotationalCipher.java
@@ -1,11 +1,42 @@
 class RotationalCipher {
+    private final int SHIFT_KEY;
+    private final StringBuilder result = new StringBuilder();
 
-    RotationalCipher(int shiftKey) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    public RotationalCipher(final int SHIFT_KEY) { this.SHIFT_KEY = SHIFT_KEY; }
+
+    public String rotate(String data) {
+        if (SHIFT_KEY == 0 || SHIFT_KEY == 26)
+            return data;
+
+        final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
+        for (int i=0; i<data.length(); i++) {
+
+            if (!Character.isLetter(data.charAt(i))) {
+                result.append(data.charAt(i));
+                continue;
+            }
+
+            if (Character.isUpperCase(data.charAt(i))) {
+                char letter = Character.toLowerCase(data.charAt(i));
+                int index = ALPHABET.indexOf(letter);
+                int actualIndex = index + SHIFT_KEY;
+
+                if (actualIndex > 25)
+                    actualIndex -= 26;
+
+                result.append(Character.toUpperCase(ALPHABET.charAt(actualIndex)));
+                continue;
+            }
+
+            int index = ALPHABET.indexOf(data.charAt(i));
+            int actualIndex = index + SHIFT_KEY;
+
+            if (actualIndex > 25)
+                actualIndex -= 26;
+
+            result.append(ALPHABET.charAt(actualIndex));
+        }
+        return result.toString();
     }
-
-    String rotate(String data) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
-
 }
