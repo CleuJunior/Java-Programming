6f4f572b949752df2b43d9dc2690db54e401d963
Cleonildo Soares Guimaraes Junior <cleu.junior@gmail.com>
2024-06-10 23:30:14 -0300

    Grep

---
+++ grep/src/main/java/GrepTool.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+class GrepTool {
+
+    String grep(String pattern, List<String> flags, List<String> files) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+}
