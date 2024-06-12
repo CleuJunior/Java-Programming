bab3465c8c11758a11ad1ed5fc45cd49f3f86ac7
Cleonildo Soares Guimaraes Junior <cleu.junior@gmail.com>
2024-06-11 23:44:02 -0300

    Alphametics

---
+++ alphametics/src/main/java/Alphametics.java
@@ -0,0 +1,13 @@
+import java.util.Map;
+
+class Alphametics {
+
+    Alphametics(String userInput) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    Map<Character, Integer> solve() throws UnsolvablePuzzleException {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+}
